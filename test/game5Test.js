b5875f3e08cb8a976f0450b24cda0965bfc41ead
jvjvalerio <julio.valerio2@live.lagcc.cuny.edu>
2022-12-26 15:10:27 -0500

    Upload entire solved directory

---
+++ test/game5Test.js
@@ -0,0 +1,49 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { assert } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Game5', function () {
+  async function deployContractAndSetVariables() {
+    const Game = await ethers.getContractFactory('Game5');
+    const game = await Game.deploy();
+
+
+    return { game };
+  }
+
+  // generate a random address which is lower than threshold
+
+  async function getAddress() {
+    const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+    let address = threshold;
+
+    while (true) {
+      const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      address = await wallet.getAddress();
+
+      if (address < threshold) {
+        return {wallet, address}
+      }
+    }
+
+  }
+
+  it('should be a winner', async function () {
+    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { wallet, address } = await getAddress();
+    console.log(`get random address, ${address}`);
+
+    const signer = ethers.provider.getSigner(0);
+    await signer.sendTransaction({to: address, value: ethers.utils.parseEther("1.0")});
+
+
+    // good luck
+    const tx = await game.connect(wallet).win();
+    console.log(`Transaction sent from address ${address}`);
+    await tx.wait();
+    console.log("Transaction completed");
+
+    // leave this assertion as-is
+    assert(await game.isWon(), 'You did not win the game');
+  });
+});
